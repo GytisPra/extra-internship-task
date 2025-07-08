44617d547e6e6d61852862c21f09e1830b01fe37
GytisPra <gpgytis@gmail.com>
2025-07-08 23:54:01 +0300

    formatting

--- src/main/python/distribution_graph.py
+++ src/main/python/distribution_graph.py
@@ -5,7 +5,14 @@ import numpy as np
 df = pd.read_json('output/top15stations.json').convert_dtypes()
 df.sort_values('name', inplace=True, key=lambda col: col.str.lower())
 
-print(f'sorted=\n{df["name"]}')
 
-plt.hist(df["passengers"], bins=10)
+R = df["passengers"].max() - df["passengers"].min() # Range
+n = len(df)                                         # Number of rows
+std = df["passengers"].std()                        # Standard deviation
+
+optimal_bin_num = np.ceil(R * ((n**(1/3)) / (3.49*std))).astype(int)
+
+plt.hist(df["passengers"], bins=optimal_bin_num, edgecolor='black')
+plt.xlabel('Passengers count')
+plt.title('Top 15 stations distribution graph (sorted by name)')
 plt.show()
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -21,19 +21,17 @@ def main(): Unit =
 
   val allStations = parseStationsFromXmlPaths(stationXmlPaths)
   val allTrains   = parseTrainsFromXmlPaths(trainsXmlPaths)
+  val allTrips    = parseTripsFromXmlPaths(tripsXmlPaths, allTrains, allStations)
 
-  val allTrips = parseTripsFromXmlPaths(tripsXmlPaths, allTrains, allStations)
-
-  val top15Stations = allStations.getTop15Stations()
-  val resultsPathTxt = os.pwd / "output" / "top15stations.txt"
+  val top15Stations   = allStations.getTop15Stations()
+  val resultsPathTxt  = os.pwd / "output" / "top15stations.txt"
   val resultsPathJson = os.pwd / "output" / "top15stations.json"
 
-  if os.exists(resultsPathTxt) then
-    os.remove(resultsPathTxt)
+  if os.exists(resultsPathTxt) then os.remove(resultsPathTxt)
 
-  if os.exists(resultsPathJson) then
-    os.remove(resultsPathJson)
+  if os.exists(resultsPathJson) then os.remove(resultsPathJson)
 
-  top15Stations.foreach(station => os.write.append(resultsPathTxt, station.toResultsString() + "\n"))
+  top15Stations.foreach(station =>
+    os.write.append(resultsPathTxt, station.toResultsString() + "\n")
+  )
   os.write.append(resultsPathJson, upickle.default.write[List[Station]](top15Stations))
-
